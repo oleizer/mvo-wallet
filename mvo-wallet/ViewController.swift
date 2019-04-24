50ee632aef2f5779b199f96ea33f50a24155cf6e
Oleg Leizer <oleizer@gmail.com>
2019-04-24 17:22:13 +0600

    Initial Commit

---
+++ mvo-wallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvo-wallet
+//
+//  Created by Олег Лейзер on 24/04/2019.
+//  Copyright © 2019 Олег Лейзер. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mvo-walletTests/mvo_walletTests.swift
@@ -0,0 +1,34 @@
+//
+//  mvo_walletTests.swift
+//  mvo-walletTests
+//
+//  Created by Олег Лейзер on 24/04/2019.
+//  Copyright © 2019 Олег Лейзер. All rights reserved.
+//
+
+import XCTest
+@testable import mvo_wallet
+
+class mvo_walletTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
